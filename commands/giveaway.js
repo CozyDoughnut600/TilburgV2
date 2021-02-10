eadbf0fedc716971c2dba606a99cf1de7ec8025d
CozyDoughnut600 <70950650+CozyDoughnut600@users.noreply.github.com>
2021-02-10 13:49:03 +0100

    V3.2
    Again bugfix giveaway

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -25,7 +25,7 @@ module.exports.run = async (bot, message, args) => {
 
     var giveawayEmbed = new discord.MessageEmbed()
         .setTitle("🎉 **Giveaway** 🎉")
-        .setFooter(`Eindigt: ${dataEnd}`)
+        .setFooter(`Eindigt: ${dateEnd}`)
         .setDescription(item)
 
 
